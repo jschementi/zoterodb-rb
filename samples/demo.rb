35c8d957c09067a264d26a2a1386d28f36eaba41
Jimmy Schementi <jschementi@gmail.com>
2009-04-22 23:25:27 -0700

    Update xbilblio and demo script

--- samples/demo.rb
+++ samples/demo.rb
@@ -54,11 +54,16 @@ def generate_items
     puts("-" * 40)
     puts "Creating \"#{it.name}\""
     it.fields.each do |field|
-      value = if field.field_type == FieldType::Url then "http://foo.com"
-      elsif field.field_type == FieldType::Location then "Garden City, NY"
-      elsif field.field_type == FieldType::Range    then "1-10"
-      elsif field.field_type == FieldType::Date     then "2009-04-19"
-      elsif field.field_type == FieldType::Name     then "James Schementi"
+      value = if field.field_type == FieldType::Url 
+        "http://foo.com"
+      elsif field.field_type == FieldType::Location
+        "Garden City, NY"
+      elsif field.field_type == FieldType::Range
+        "1-10"
+      elsif field.field_type == FieldType::Date
+        "2009-04-19"
+      elsif field.field_type == FieldType::Name
+        $names[rand($names.size)]
       else
         field.name
       end
@@ -80,29 +85,74 @@ def generate_items
   end
 end
 
-def format_items
-  puts "Formatting items ...\n"
-  text = ''
-  Style.all.each do |s|
-    puts text = "<h1>#{s.id.upcase}</h1>"
-    Item.all.each do |i|
-      [:bibliography, :note].each do |t|
-        text = <<-EOS
-<h2>#{i.item_type.name} (#{s.id} #{t})</h2>
-#{s.format(i, t, :html)}
-        EOS
-        puts text
-      end
+def render_item(i, file)
+  output file, "    <div style='border-left: 10px solid lightgray; padding-left: 10px'>\n"
+  output file, "      <table>\n"
+  i.item_type.fields.each do |f|
+    begin
+      output file, "        <tr><td><b>#{f.display_name}</b></td><td> #{i.send(f.name)}</td></tr>\n"
+    rescue
     end
-    puts text = "<hr />"
   end
-  #puts text
+  i.item_type.item_type_creator_types.each do |itct|
+    name = itct.creator_type.name
+    output file, "        <tr><td><b>#{name}</b></td><td>#{
+      [i[name]].flatten.map do |ic| 
+        ic.creator.creator_data.full_name
+      end.join(" | ")
+    }</td></tr>\n"
+  end
+  output file, "      </table>\n"
+  output file, "    </div>\n"
 end
 
+def format_items(f)
+  puts "Formatting items ..."
+  [Style.find('mla')].each do |s|
+    output f, "  <h1>#{s.id.upcase}</h1>\n"
+    Item.all.each do |i|
+      output f, "    <h2>#{i.item_type.display_name}</h2>\n"
+      render_item(i, f)
+      output f, "    <div style='border-left: 10px solid lightgray; padding-left: 10px'>\n"
+      [:bibliography, :note].each do |t|
+        txt = s.format(i, t, :html)
+        txt = txt.strip == "<div></div>" ? '<div><em>No format</em></div>' : txt
+        output f, "      <h3>#{t}</h3>\n      #{txt}"
+      end
+      output f, "    </div>\n"
+    end
+    output f, "<hr />\n"
+  end
+end
+
+def output(f, text)
+  print text
+  f.print text if f
+end
+
+$actions = {
+  :init => lambda { start },
+  :test => lambda do
+    f = File.open(File.dirname(__FILE__) + '/demo.html', 'w') if $actions[:output?][]
+    output f, "<html>\n  <head>\n    <title>MLA</title>\n  </head>\n  <body>\n"
+    format_items(f)
+    output f, "  </body>\n</html>\n"
+    f.close if f
+  end,
+  :rebuild => lambda do
+    if $actions[:rebuild?][]
+      rebuild_database
+      copy_database
+    end
+  end,
+  :output?  => lambda { return ARGV.include?('output') },
+  :rebuild? => lambda { return ARGV.include?('rebuild') },
+  :generate => lambda { return generate_items if $actions[:rebuild?][] }
+}
+
 if __FILE__ == $0
-  #rebuild_database
-  #copy_database
-  start
-  #generate_items
-  format_items
+  $actions[:rebuild][] 
+  $actions[:init][]
+  $actions[:generate][] 
+  $actions[:test][]
 end
